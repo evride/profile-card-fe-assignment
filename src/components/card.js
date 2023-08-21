09c7ad55f13e21ce6abd9fb684b3e1f42b472aba
Evan Ridenour <evan@every-mother.com>
2023-08-21 17:17:40 -0400

    Finish implementing card, responsive styles, microdata

--- src/components/card.js
+++ src/components/card.js
@@ -3,6 +3,8 @@ export default class Card {
 		this.profile = profile;
 		this._element = document.createElement('div');
 		this._element.classList.add('card');
+		this._element.setAttribute('itemscope', '');
+		this._element.setAttribute('itemtype', 'https://schema.org/Person');
 		this.render();
 	}
 	render() {
@@ -19,14 +21,17 @@ export default class Card {
 
 		const thumbImg = document.createElement('img');
 		thumbImg.src = window._getThumbnailURL(this.profile.thumbnail);
-		thumbImg.title = this.profile.icaName;
-
+		thumbImg.title = `${this.profile.icaName} - ${this.profile.title}`;
+		thumbImg.alt = this.profile.icaName;
+		thumbImg.setAttribute('itemprop', 'photo');
 
 		const fullnameElem = document.createElement('h2');
 		fullnameElem.innerText = this.profile.icaName;
+		fullnameElem.setAttribute('itemprop', 'name');
 
 		const jobTitleElem = document.createElement('h3');
 		jobTitleElem.innerText = this.profile.title;
+		jobTitleElem.setAttribute('itemprop', 'title');
 
 		const bioElem = document.createElement('p');
 		bioElem.classList.add('bio');
@@ -37,22 +42,37 @@ export default class Card {
 		contactBtn.classList.add('contact', 'btn');
 		contactBtn.innerText = "Contact";
 		contactBtn.href = `mailto:${this.profile.email}`;
+		contactBtn.setAttribute('alt', this.profile.email);
+
+		const skillCont = document.createElement('div');
+		const skillInfo = document.createElement('span');
+		const skillLabel = document.createElement('label');
+		skillCont.classList.add('info');
+		skillInfo.innerText = `${this.profile.linkedInSkill},`;
+		skillLabel.innerText = "Skill";
+		skillCont.appendChild(skillInfo);
+		skillCont.appendChild(skillLabel);
+
+		const teamCont = document.createElement('div');
+		const teamInfo = document.createElement('span');
+		const teamLabel = document.createElement('label');
+		teamCont.classList.add('info');
+		teamInfo.innerText = `${this.profile.teamName},`;
+		teamInfo.setAttribute('itemprop', 'organizational-unit');
+		teamLabel.innerText = "Team";
+		teamCont.appendChild(teamInfo);
+		teamCont.appendChild(teamLabel);
 
 		thumbCont.appendChild(thumbImg);
 		cardInner.appendChild(thumbCont);
 		cardInner.appendChild(fullnameElem);
 		cardInner.appendChild(jobTitleElem);
 		cardInner.appendChild(bioElem);
+		cardInner.appendChild(skillCont);
+		cardInner.appendChild(teamCont);
 		cardInner.appendChild(contactBtn);
 
 		this._element.appendChild(cardInner);
-	}
-	renderByTemplate() {
-
-	}
-	convertTemplate() {
-
-		return new Function('state', 'return `' + cardHtml + '`;')({profile});
 	}
 	get element() {
 		return this._element;
